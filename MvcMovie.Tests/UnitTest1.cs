a00257d128b1a34538ef4d1df4544644f4aa6a74
Doodle <23706830+cafedoodle@users.noreply.github.com>
2025-12-11 15:17:58 -0600

    initial check in

---
+++ MvcMovie.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MvcMovie.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        //[Fact]
+        //public void Test2()
+        //{
+        //    Assert.True(false);
+        //}
+    
+    }
+}
+
+
+
