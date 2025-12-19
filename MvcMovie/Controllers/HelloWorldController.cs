b695569b3758b446219794a36db323e2984709d3
cafedoodle <23706830+cafedoodle@users.noreply.github.com>
2025-12-19 12:20:15 -0600

    Feature002 - Add sample Issue (#13)
    * add feature 3
    
    * add feature 4
    
    * Add sample issue of Hardcoded Secrets

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -18,4 +18,35 @@ public class HelloWorldController : Controller
         ViewData["NumTimes"] = numTimes;
         return View();
     }
-}
+
+    //
+    // GET: User/GetUserData/{username}  
+    public async Task<ActionResult> GetUserData(string username)
+    {
+        var userData = await FetchUserDataAsync(username);
+        return Content(userData, "application/json");
+    }
+
+    private async Task<string> FetchUserDataAsync(string username)
+    {
+        // Hardcoded secret (API key)  
+        string apiKey = "12345-ABCDE-67890-FGHIJ";
+
+        string url = $"https://api.example.com/users/{username}?api_key={apiKey}";
+
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return "{\"error\": \"User not found\"}";
+            }
+        }
+    }
+    //
+
+ }
